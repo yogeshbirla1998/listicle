abcc42c97762dc4feb98f5d51bf204678b7dc1c8
Yogesh Birla <yogeshbirla@1s-MacBook-Pro.local>
2023-08-11 02:10:26 -0700

    Added navigation between screens This commit contains - navigation between different screens through links & buttons - bottom tab navition between screens

--- App.js
+++ App.js
@@ -1,24 +1,92 @@
 import React, { useEffect } from 'react';
-import { SafeAreaView } from 'react-native';
 import Signin from './src/screens/auth/Signin';
+import Splash from './src/screens/auth/Splash';
+import Signup from './src/screens/auth/Signup';
+import Home from './src/screens/app/Home';
+import Favorites from './src/screens/app/Favorites';
+import Profile from './src/screens/app/Profile';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import Config from "react-native-config";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { colors } from './src/utils/colors';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Image } from 'react-native';
 
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => (
+  <Tab.Navigator 
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let icon;
+
+        if (route.name === 'Home') {
+          icon = focused
+            ? require('./src/assets/tabs/home_active.png')
+            : require('./src/assets/tabs/home.png');
+        } else if (route.name === 'Profile') {
+          icon = focused
+            ? require('./src/assets/tabs/profile_active.png')
+            : require('./src/assets/tabs/profile.png');
+        } else if (route.name === 'Favorites') {
+          icon = focused
+            ? require('./src/assets/tabs/bookmark_active.png')
+            : require('./src/assets/tabs/bookmark.png');
+        }
+
+        // You can return any component that you like here!
+        return <Image style={{ width: 24, height: 24 }} source={icon} />
+      },
+      headerShown: false,
+      tabBarShowLabel: false,
+      tabBarStyle: { backgroundColor: colors.lightGrey, },
+    })}
+  >
+    <Tab.Screen name="Home" component={Home} />
+    <Tab.Screen name="Favorites" component={Favorites} />
+    <Tab.Screen name="Profile" component={Profile} />
+  </Tab.Navigator>
+)
 
 const App = () => {
+  const isSignedIn = true;
 
   useEffect(() => {
     GoogleSignin.configure({
       scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
       webClientId: Config.GOOGLE_WEB_CLIENT_ID, // client ID of type WEB for your server (needed to verify user ID and offline access)
       offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      iosClientId: Config.GOOGLE_IOS_CLIENT_ID, // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
     });
   }, [])
 
+  const theme = {
+    colors: {
+      background: colors.white,
+    }
+  }
+
   return (
-    <SafeAreaView>
-      <Signin />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <NavigationContainer theme={theme}>
+        <Stack.Navigator>
+          {isSignedIn ? (
+            <>
+              <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="Splash" component={Splash} options={{ headerShown: false }} />
+              <Stack.Screen name="Signin" component={Signin} options={{ headerShown: false }} />
+              <Stack.Screen name="Signup" component={Signup} options={{ headerShown: false }} />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
--- src/screens/auth/Splash/index.js
+++ src/screens/auth/Splash/index.js
@@ -2,24 +2,36 @@ import React from 'react';
 import { Text, Image, View, Pressable } from 'react-native';
 import Button from '../../../components/Button';
 import { styles } from './styles';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const Splash = () => {
+const Splash = ({navigation}) => {
+
+    const onSignin = () =>{
+        navigation.navigate('Signin')
+    }
+
+    const onSignup = () =>{
+        navigation.navigate('Signup')
+    }
+
     return (
-        <View style={styles.container}>
-            <Image resizeMode='contain' style={styles.image} source={require('../../../assets/splash_image.png')} />
+        <SafeAreaView>
+            <View style={styles.container}>
+                <Image resizeMode='contain' style={styles.image} source={require('../../../assets/splash_image.png')} />
 
-            <View style={styles.titleContainer}>
-                <Text style={styles.title}>You'll Find</Text>
-                <Text style={[styles.title, styles.innerTitle]}>All you need</Text>
-                <Text style={styles.title}>Here!</Text>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>You'll Find</Text>
+                    <Text style={[styles.title, styles.innerTitle]}>All you need</Text>
+                    <Text style={styles.title}>Here!</Text>
+                </View>
+
+                <Button onPress={onSignup} title="Sign Up" />
+
+                <Pressable hitSlop={20}>
+                    <Text onPress={onSignin} style={styles.footerText}>Sign In</Text>
+                </Pressable>
             </View>
-
-            <Button title="Sign Up" />
-
-            <Pressable hitSlop={20}>
-                <Text style={styles.footerText}>Sign In</Text>
-            </Pressable>
-        </View>
+        </SafeAreaView>
     )
 }
 
